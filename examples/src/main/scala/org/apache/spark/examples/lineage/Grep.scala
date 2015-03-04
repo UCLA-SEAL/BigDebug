260bafb6761f39bb6b42c811fed9a7629b792e24
shrinidhihudli <shrinidhi@hudli.com>
2015-03-04 12:01:03 -0800

    Instrumented Grep with Lineage

--- examples/src/main/scala/org/apache/spark/examples/lineage/Grep.scala
+++ examples/src/main/scala/org/apache/spark/examples/lineage/Grep.scala
@@ -9,17 +9,27 @@ import org.apache.spark.{SparkConf, SparkContext}
 object Grep {
 
 	def main(args: Array[String]): Unit = {
-    //val logFile = "README.md"
-    val logFile = "../../datasets/output10.txt"
     val conf = new SparkConf()
-      .setMaster("local[2]")
-      .setAppName("Grep")
+    var lineage = false
+    var logFile = "hdfs://scai01.cs.ucla.edu:9000/clash/"
+    if(args.size < 2) {
+      //logFile = "../../datasets/output.txt"
+      //val logFile = "../../datasets/data-MicroBenchmarks/lda_wiki1w_2"
+      logFile = "README.md"
+      conf.setMaster("local[2]")
+      lineage = true
+    } else {
+      lineage = args(0).toBoolean
+      logFile += args(1)
+      conf.setMaster("spark://SCAI01.CS.UCLA.EDU:7077")
+    }
+    conf.setAppName("Grep-" + lineage + "-" + logFile)
     val sc = new SparkContext(conf)
     val lc = new LineageContext(sc)
-    lc.setCaptureLineage(true)
+    lc.setCaptureLineage(lineage)
     val lines = lc.textFile(logFile, 2)
     val result = lines.filter(line => line.contains("spark"))
-    print(result.collect().size)
+    print(result.count())
 		//println(result.collect().mkString("\n"))
 
 //    lc.setCaptureLineage(false)
