eef6ee5223fb6f0fb018da142add9b8296646503
jteoh <jiateoh@gmail.com>
2019-02-14 15:32:25 -0800

    Edit Web UI to specify PerfDebug for easier distinguishment.

--- core/src/main/scala/org/apache/spark/deploy/master/ui/MasterPage.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/ui/MasterPage.scala
@@ -170,7 +170,7 @@ private[ui] class MasterPage(parent: MasterWebUI) extends WebUIPage("") {
           }
         </div>;
 
-    UIUtils.basicSparkPage(content, "Spark Master at " + state.uri)
+    UIUtils.basicSparkPage(content, "Spark-PerfDebug Master at " + state.uri)
   }
 
   private def workerRow(worker: WorkerInfo): Seq[Node] = {
